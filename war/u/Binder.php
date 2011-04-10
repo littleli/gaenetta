fc229492e7630e7356f10c07d254b8fc3d2c8b3a
Ales Najmann <ales.najmann@gmail.com>
2011-04-10 22:44:44 +0200

    Binder argument simplification

--- war/u/Binder.php
+++ war/u/Binder.php
@@ -2,10 +2,11 @@
 
 class Binder {
 
-  private static $log = java_class("java.util.logging.Logger")->getLogger(__CLASS__);
+  private static $log = java_class('java.util.logging.Logger')->getLogger(__CLASS__);
   private $rejected;
   private $accepted;
   private $target;
+  private $params;
 
   public function getRejected() {
     return $this->rejected;
@@ -18,17 +19,22 @@ class Binder {
   public function getTarget() {
     return $this->target;
   }
+
+  public function getParams() {
+    return $this->params;
+  }
   
-  protected function __construct(&$target) {
+  protected function __construct(&$target, &$params) {
     $this->target = $target;
-  	$this->rejected = array();
-  	$this->accepted = array();
+    $this->params = $params;
+    $this->rejected = array();
+    $this->accepted = array();
   }
 
   public static function bindData(&$target, $mapping, $params) {
-    $result = new Binder($target);
+    $result = new Binder($target, $params);
     foreach ($mapping as $field => $validators) {
-      $result->validateAndBind($target, $field, $validators, $params[$field]);
+      $result->validateAndBind($field, $validators, $params[$field]);
     }
     return $result;
   }
@@ -44,7 +50,7 @@ class Binder {
     }
   }
 
-  private function validateAndBind(&$target, $field, $validators, $value) {
+  private function validateAndBind($field, $validators, $value) {
     $allValid = true;
     $bindingValidator = false;
 
@@ -66,21 +72,33 @@ class Binder {
       if ($bindingValidator) {
         $convertedValue = $bindingValidator->convert($value);
       } else {
-        $convertedValue = $value; // defaults to string
+        $convertedValue = $value; // defaults to string, no binding validator found in chain
       }
-      if ($convertedValue !== NULL) {
-	    $this->bindOnTarget($target, $field, $convertedValue);
-	  }
+      if ($convertedValue != NULL) {
+	    $this->bindOnTarget($field, $convertedValue);
+      }
     }
   }
 
-  private function bindOnTarget(&$target, $field, $value) {
-    if (is_object($target)) {
-      $target->$field = $value;
-    } elseif (is_array($target)) {
-      $target[$field] = $value;
+  private function bindOnTarget($field, $value) {
+    if (is_object($this->target)) {
+      $this->target->$field = $value;
+    } elseif (is_array($this->target)) {
+      $this->target[$field] = $value;
     } else {
-       self::$log->error("Value [$value] could not be bound to field [$field]");
+      self::$log->error("Value [$value] could not be bound to field [$field]");
     }
+  }
+  
+  public function markRejected(&$attributes) {
+    if ($this->hasErrors()) {
+      $name = $attributes['name'];
+      $attributes['value'] = $this->rejected[$name]['value'];
+      $class = $attributes['class'];
+      if ($class) {
+        $attributes['class'] = "$class error";
+      }
+    }
+    return $attributes;
   }
 }
